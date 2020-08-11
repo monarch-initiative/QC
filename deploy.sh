9a6e97aeeaff18dfb78a79ef180b2c5ba6945560
kshefchek <kshefchek@gmail.com>
2020-08-11 13:12:49 -0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -12,15 +12,14 @@ cd docs/.vuepress/dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-# git init
-git add -A
+git init
+git add .
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f origin master:gh-pages
+
 cd -
-git push -f git@github.com:monarch-initiative/QC.git gh-pages:gh-pages
-
-
