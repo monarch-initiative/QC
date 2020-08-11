d9a95a4aac2b12f6a4d9a7028b97a3f02dd8e83e
kshefchek <kshefchek@gmail.com>
2020-08-11 12:56:41 -0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ cd docs/.vuepress/dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+# git init
 git add -A
 git commit -m 'deploy'
 
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:monarch-initiative/qc.git master:gh-pages
-
 cd -
+git push -f git@github.com:monarch-initiative/QC.git gh-pages:gh-pages
+
+
